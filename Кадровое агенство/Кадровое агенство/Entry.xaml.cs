a747a83a69c081097c24d7e64997046b2e26a86d
ElizarovRoman <77525760+ElizarovRoman@users.noreply.github.com>
2021-06-01 17:43:48 +0400

    Add files via upload

---
+++ Кадровое агенство/Кадровое агенство/Entry.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Кадровое_агенство
+{
+    /// <summary>
+    /// Логика взаимодействия для Entry.xaml
+    /// </summary>
+    public partial class Entry : Window
+    {
+        public Entry()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (log_vhod.Text.Length > 0)
+            {
+                if (pass_vhod.Password.Length > 0)
+                {
+                    MainWindow mainw = new MainWindow();
+                    mainw.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Введите пароль", "Ошибка входа",MessageBoxButton.OK,MessageBoxImage.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите логин", "Ошибка входа", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+    }
+}
---
+++ Кадровое агенство/Кадровое агенство/General.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Кадровое_агенство
+{
+    /// <summary>
+    /// Логика взаимодействия для General.xaml
+    /// </summary>
+    public partial class General : Window
+    {
+        public General()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Entry entrywin = new Entry();
+            entrywin.Show();
+            this.Close();
+        }
+
+        private void ToREG_Click(object sender, RoutedEventArgs e)
+        {
+            Registration regwin = new Registration();
+            regwin.Show();
+            this.Close();
+        }
+    }
+}
